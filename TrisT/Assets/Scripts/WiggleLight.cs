46ca21166ca7729427e67d5018c8dc182a05045e
Lasse <116916398+LazeIsCoding@users.noreply.github.com>
2023-11-24 20:20:50 +0100

    Push Push Push

--- TrisT/Assets/Scripts/WiggleLight.cs
+++ TrisT/Assets/Scripts/WiggleLight.cs
@@ -2,20 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CursorLight : MonoBehaviour
+public class WiggleLight : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        transform.position = new Vector3(cursorPos.x, cursorPos.y, -20.0f);
-
-
+        
     }
 }
