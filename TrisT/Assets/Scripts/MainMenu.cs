6ae8c427da16e0a1f1cbb35d31ce03ccc51be318
TheFefel <flxkhn0602@gmail.com>
2023-11-25 22:51:21 +0100

    SceneChanger + Optionsmenu Start

--- TrisT/Assets/Scripts/MainMenu.cs
+++ TrisT/Assets/Scripts/MainMenu.cs
@@ -1,25 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(1);
         Cursor.visible = false;
     }
 
-    public void Options()
-    {
-        SceneManager.LoadSceneAsync(2);
-    }
-
-    public void Achievements()
-    {
-        SceneManager.LoadSceneAsync(3);
-    }
     public void Quit()
     {
         Application.Quit();
---
+++ TrisT/Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SearchService;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private static int a;
+    private static Stack<int> sceneHistory = new Stack<int>();
+
+    public void PlayGame()
+    {
+        sceneHistory.Push(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(1);
+    }
+
+    public void Options()
+    {
+        sceneHistory.Push(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(2);
+    }
+
+    public void Achievements()
+    {
+        sceneHistory.Push(SceneManager.GetActiveScene().buildIndex);
+        SceneManager.LoadScene(3);
+    }
+
+    public void PreviousScene()
+    {
+        SceneManager.LoadSceneAsync(sceneHistory.Pop());
+    }
+}
