c5180a3742f69b343665b3312d6957e7f5ad1b6f
Lasse <116916398+LazeIsCoding@users.noreply.github.com>
2023-11-25 12:49:36 +0100

    Ka was ich gemacht hab

--- TrisT/Assets/Scripts/CursorLight.cs
+++ TrisT/Assets/Scripts/CursorLight.cs
@@ -8,16 +8,28 @@ public class CursorLight : MonoBehaviour
     private Light _myLight;
     private float timer = 0;
     [SerializeField] private float speed = 6;
+    private Color color1;
+    private Color color2;
     
     // Start is called before the first frame update
     void Start()
     {
         //Cursor.visible = false; für Felix
         _myLight = GetComponent<Light>();
+        
+        //_myLight.color = new Color(249, 217,123, 255);
+        /*color1 = new Color(249.0f / 255.0f, 211.0f / 255.0f, 123.0f / 255.0f, 0.7f);
+        color2 = new Color(249.0f / 255.0f, 211.0f / 255.0f, 123.0f / 255.0f, 1);*/
     }
     
     void Update()
     {
+        /*double Dtime = (double) Time.time;
+        double test = (Math.Sin(Dtime * 3) + 1) /2;
+        float amp = 10;*/
+        //_myLight.color = Color.Lerp(color1, color2, (float)test);
+        
+        
         Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         transform.position = new Vector3(cursorPos.x, cursorPos.y, -20.0f);
         timer += 0.001f;
