ce7d68fc3187a50ac0b9d80e4ad899b6fab0acf6
Christopher Liebsch <103792360+chrksa@users.noreply.github.com>
2023-11-30 23:06:42 +0100

    Grundstein new Gamesystem -> Objects

--- TrisT/Assets/Scripts/GameManager.cs
+++ TrisT/Assets/Scripts/GameManager.cs
@@ -7,12 +7,17 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Todo: init methods
     }
 
     // Update is called once per frame
     void Update()
     {
+        
+        /*  Todo: Lifecicle
+            1: Check for row
+            2: ...
+        */ 
         
     }
 }
